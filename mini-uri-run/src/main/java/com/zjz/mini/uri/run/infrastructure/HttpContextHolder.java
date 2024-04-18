defa5f968cab58b0a83978224b4d55897babcb23
hkz329 <190022707@qq.com>
2024-04-19 04:08:01 +0800

    Filter

--- mini-uri-run/src/main/java/com/zjz/mini/uri/run/infrastructure/HttpContextHolder.java
+++ mini-uri-run/src/main/java/com/zjz/mini/uri/run/infrastructure/HttpContextHolder.java
@@ -1,32 +1,32 @@
 package com.zjz.mini.uri.run.infrastructure;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 
 public class HttpContextHolder {
 
-    private static final ThreadLocal<HttpServletRequest> REQUEST_THREAD_LOCAL_HOLDER = new ThreadLocal<>();
-    private static final ThreadLocal<HttpServletResponse> RESPONSE_THREAD_LOCAL_HOLDER = new ThreadLocal<>();
+    private static final ThreadLocal<ServletRequest> REQUEST_THREAD_LOCAL_HOLDER = new ThreadLocal<>();
+    private static final ThreadLocal<ServletResponse> RESPONSE_THREAD_LOCAL_HOLDER = new ThreadLocal<>();
 
     public static void remove() {
         REQUEST_THREAD_LOCAL_HOLDER.remove();
         RESPONSE_THREAD_LOCAL_HOLDER.remove();
     }
 
-    public static HttpServletResponse getHttpResponse() {
+    public static ServletResponse getHttpResponse() {
 
         return RESPONSE_THREAD_LOCAL_HOLDER.get();
     }
 
-    public static void setHttpResponse(HttpServletResponse response) {
+    public static void setHttpResponse(ServletResponse response) {
         RESPONSE_THREAD_LOCAL_HOLDER.set(response);
     }
 
-    public static HttpServletRequest getHttpRequest() {
+    public static ServletRequest getHttpRequest() {
         return REQUEST_THREAD_LOCAL_HOLDER.get();
     }
 
-    public static void setHttpRequest(HttpServletRequest request) {
+    public static void setHttpRequest(ServletRequest request) {
 
         REQUEST_THREAD_LOCAL_HOLDER.set(request);
     }
--- mini-uri-run/src/main/java/com/zjz/mini/uri/run/infrastructure/config/RequestFilter.java
+++ mini-uri-run/src/main/java/com/zjz/mini/uri/run/infrastructure/config/RequestFilter.java
@@ -1,33 +1,49 @@
 package com.zjz.mini.uri.run.infrastructure.config;
 
+import com.zjz.mini.uri.run.infrastructure.HttpContextHolder;
 import jakarta.servlet.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 /**
  * 请求过滤
  * @author hkz329
  */
+@Slf4j
+@Component
 public class RequestFilter implements Filter {
 
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // RequestFilter 实例化的时候调用此方法，进行加载filterConfig
+    }
 
     /**
      * OncePerRequestFilter 对比
      */
 
-
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        Filter.super.init(filterConfig);
-    }
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain){
+        try {
+            LocalDateTime start = LocalDateTime.now();
+            log.info("doFilter start,Time:{}",start);
+            HttpContextHolder.setHttpRequest(servletRequest);
+            HttpContextHolder.setHttpResponse(servletResponse);
+            filterChain.doFilter(servletRequest, servletResponse);
+            log.info("doFilter end,Time:{}, consume:{}", LocalDateTime.now(), Duration.between(start, LocalDateTime.now()).toMillis());
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }finally {
+            HttpContextHolder.remove();
+        }
     }
 
     @Override
     public void destroy() {
-        Filter.super.destroy();
+
     }
 }
