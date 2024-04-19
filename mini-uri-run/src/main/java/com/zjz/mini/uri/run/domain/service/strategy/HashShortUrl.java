a91cef5b6bb24967692efccc755317696a015418
hkz329 <190022707@qq.com>
2024-04-19 14:46:49 +0800

    feat: 缓存时间

--- mini-uri-run/src/main/java/com/zjz/mini/uri/run/domain/service/strategy/HashShortUrl.java
+++ mini-uri-run/src/main/java/com/zjz/mini/uri/run/domain/service/strategy/HashShortUrl.java
@@ -11,6 +11,7 @@ import com.zjz.mini.uri.run.domain.entity.UrlMapping;
 import com.zjz.mini.uri.run.domain.service.ShortUrlBase;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -30,6 +31,8 @@ public class HashShortUrl extends ShortUrlBase {
     //自定义长链接防重复字符串
     private static final String DUPLICATE = "$";
 
+    @Value("${miniuri.cache.timeout}")
+    private Long TIMEOUT;
     //创建布隆过滤器
     private static final BitMapBloomFilter FILTER = BloomFilterUtil.createBitMap(10);
 
@@ -55,18 +58,25 @@ public class HashShortUrl extends ShortUrlBase {
         return encode;
     }
 
+    /**
+     * 处理逻辑
+     * @param shortUrl 短链接
+     * @param longUrl  长链接
+     * @param originUrl 初始长链接
+     * @return
+     */
     @Override
     protected String doProcess(String shortUrl, String longUrl, String originUrl) {
         if (shortUrl.length() == 1) {
             longUrl += DUPLICATE;
             shortUrl = doProcess(toEncode(toHash(longUrl)), longUrl, originUrl);
-        } else if (FILTER.contains(shortUrl)) {
+        } else if (FILTER.contains(shortUrl)) { // 防止 hash 冲突
             // 此种情况布隆过滤器存在误判，即布隆过滤器中可能存在也可能不存在该key
             // 布隆过滤器中存在，查redis 缓存
             String redisLongUrl = this.redisTemplate.opsForValue().get(shortUrl) + "";
             if (originUrl.equals(redisLongUrl)) {
                 // 重置缓存过期时间
-                this.redisTemplate.expire(shortUrl, 1, TimeUnit.HOURS);
+                this.redisTemplate.expire(shortUrl, TIMEOUT, TimeUnit.HOURS);
                 return shortUrl;
             }
             // 没有缓存,重新hash
@@ -81,9 +91,8 @@ public class HashShortUrl extends ShortUrlBase {
             super.addUrlMapping(urlMapping);
             FILTER.add(shortUrl);
             // 存缓存
-            // fixme 此处 缓存时间先写死
             log.info("start to insert into redis,data:{}", JSONUtil.toJsonStr(urlMapping));
-            this.redisTemplate.opsForValue().set(shortUrl, originUrl, 1, TimeUnit.HOURS);
+            this.redisTemplate.opsForValue().set(shortUrl, originUrl, TIMEOUT, TimeUnit.HOURS);
         }
 
         return shortUrl;
@@ -97,9 +106,11 @@ public class HashShortUrl extends ShortUrlBase {
             return longUrl;
         }
         UrlMapping urlMapping = super.getByShortUrl(shortUrl);
+        longUrl = urlMapping.getLongUrl();
         if (null != urlMapping.getLongUrl()) {
-
+            //数据库有此短链接，添加缓存
+           this.redisTemplate.opsForValue().set(shortUrl, longUrl, TIMEOUT, TimeUnit.HOURS);
         }
-        return urlMapping.getLongUrl();
+        return longUrl;
     }
 }
