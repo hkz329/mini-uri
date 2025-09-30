92707e857f9ce7b23341c75a3f0cf113db4a3fa3
hkz329 <190022707@qq.com>
2025-09-30 17:15:24 +0800

    @Range 校验

--- mini-uri-run/src/main/java/com/zjz/mini/uri/run/domain/dto/GenerateUrlReq.java
+++ mini-uri-run/src/main/java/com/zjz/mini/uri/run/domain/dto/GenerateUrlReq.java
@@ -2,8 +2,8 @@ package com.zjz.mini.uri.run.domain.dto;
 
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
 
 
 /**
@@ -21,5 +21,6 @@ public class GenerateUrlReq {
     /**
      * 过期时间默认 1天
      */
+    @Range(min = 1, max = 90, message = "过期时间必须在 1-90 天之间")
     private Integer expireTime = 1;
 }
