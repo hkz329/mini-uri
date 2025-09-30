eabd8e02dc55e81732a20d8908f738b19f6a2755
hkz329 <190022707@qq.com>
2025-09-30 17:13:09 +0800

    GlobalExceptionHandler

--- mini-uri-run/src/main/java/com/zjz/mini/uri/run/infrastructure/config/GlobalExceptionHandler.java
+++ mini-uri-run/src/main/java/com/zjz/mini/uri/run/infrastructure/config/GlobalExceptionHandler.java
@@ -4,8 +4,12 @@ import com.zjz.mini.uri.framework.common.core.BusinessException;
 import com.zjz.mini.uri.framework.common.core.R;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 /**
  * 全局异常处理
@@ -17,8 +21,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
-    public R handleBusException(HttpServletRequest request, BusinessException ex) {
+    public R<?> handleBusException(HttpServletRequest request, BusinessException ex) {
         log.error("process url {} failed", request.getRequestURL().toString(), ex);
         return R.fail(500, ex.getMsg());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public R<?> handleParamsValidationException(HttpServletRequest request, MethodArgumentNotValidException ex) {
+        String message = ex.getBindingResult().getAllErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage).collect(Collectors.joining());
+        return R.fail(500, message);
     }
 }
